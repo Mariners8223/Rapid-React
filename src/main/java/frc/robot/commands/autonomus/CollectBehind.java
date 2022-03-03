3c93c60abe8c03f884263db09f0acf50aa44b9cd
EdenGranot <eden.granot@gmail.com>
2022-03-03 19:02:29 +0200

    moved the path follower command

--- src/main/java/frc/robot/commands/autonomus/CollectBehind.java
+++ src/main/java/frc/robot/commands/autonomus/CollectBehind.java
@@ -2,6 +2,7 @@ package frc.robot.commands.autonomus;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.robot.Constants;
+import frc.robot.commands.PathFollower;
 import frc.robot.commands.mechanisems.IntakeBalls;
 
 public class CollectBehind extends ParallelCommandGroup {
--- src/main/java/frc/robot/commands/autonomus/OneBallAuto.java
+++ src/main/java/frc/robot/commands/autonomus/OneBallAuto.java
@@ -2,6 +2,7 @@ package frc.robot.commands.autonomus;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants;
+import frc.robot.commands.PathFollower;
 import frc.robot.commands.mechanisems.ShootCycle;
 
 public class OneBallAuto extends SequentialCommandGroup {
