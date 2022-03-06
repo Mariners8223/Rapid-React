e98d4ef46dee9fe6d31ab196c75ae9576a997935
EdenGranot <eden.granot@gmail.com>
2022-03-06 20:32:06 +0200

    switched back to old shooting command

--- src/main/java/frc/robot/commands/mechanisems/ShootCycle.java
+++ src/main/java/frc/robot/commands/mechanisems/ShootCycle.java
@@ -38,8 +38,9 @@ public class ShootCycle extends CommandBase {
     }
     else {
       shooter.setSpeed(Math.abs(RobotContainer.getArmsAxis(Constants.SHOOT_TRIGGER)) * Constants.SHOOTER_SPEED);
-      if (RobotContainer.getArmsButton(Constants.TRANSPORT_OUTWARDS_BUTTON)) transport.transportOutwards(Constants.TRANSPORT_SPEED);
-      else transport.transportInwards(Constants.TRANSPORT_SPEED);
+      if (RobotContainer.getArmsButton(Constants.TRANSPORT_INWARDS_BUTTON)) transport.transportInwards(Constants.TRANSPORT_SPEED);
+      else if (RobotContainer.getArmsButton(Constants.TRANSPORT_OUTWARDS_BUTTON)) transport.transportOutwards(Constants.TRANSPORT_SPEED);
+      else transport.stopAll();
     }
   }
 
