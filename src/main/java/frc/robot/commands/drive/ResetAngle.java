229cabe8755ae30b885b56edb68e50df5e3375e5
EdenGranot <eden.granot@gmail.com>
2022-03-06 12:43:48 +0200

    implemented

---
+++ src/main/java/frc/robot/commands/drive/ResetAngle.java
@@ -0,0 +1,28 @@
+package frc.robot.commands.drive;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Chassis;
+
+public class ResetAngle extends CommandBase {
+  private Chassis chassis;
+  public ResetAngle() {
+    chassis = Chassis.getInstance();
+    addRequirements(chassis);
+  }
+
+  @Override
+  public void initialize() {
+    chassis.resetAngle();
+  }
+
+  @Override
+  public void execute() {}
+
+  @Override
+  public void end(boolean interrupted) {}
+
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
